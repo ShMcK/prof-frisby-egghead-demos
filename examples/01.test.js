0a8d7d217431ab942e0774d3396c41cd3f9f5527
ShMcK <shawn.j.mckay@gmail.com>
2016-12-11 13:24:44 -0800

    01: box - map & fold

---
+++ examples/01.test.js
@@ -0,0 +1,29 @@
+// const nextCharForNumberString = str => {
+//   const trimmed = str.trim()
+//   const number = parseInt(trimmed)
+//   const nextNumber = number + 1
+//   return String.fromCharCode(nextNumber)
+// }
+
+// const nextCharForNumberString = str => {
+//   return String.fromCharCode(parseInt(str.trim()) + 1)
+// }
+
+const Box = require('../lib')
+
+const nextCharForNumberString = str => 
+  Box(str)
+  .map(s => s.trim())
+  .map(s => new Number(s))
+  .map(s => s + 1)
+  .map(s => String.fromCharCode(s))
+  .fold(s => s.toLowerCase())
+
+
+describe('01', () => {
+  it('should work', () => {
+    const result = nextCharForNumberString('  64  ')
+    expect(result).toBe('a')
+  })
+})
+  
